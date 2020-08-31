b93e53b261d69c02cd0341f0ecf932788ce5c7e9
Samuel Smith <63981393+samsmi84@users.noreply.github.com>
2020-08-31 14:54:53 +1200

    Fixed Bugs in HTML, CSS, and JS files

---
+++ Script/script.js
@@ -0,0 +1,39 @@
+// Changed the square brackets '[]' to brackets '()'
+function debounce (func, wait = 20, immediate = true) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
+// changed slidr to slider
+
+const sliderImages = document.querySelectorAll('#slide-in');
+
+function checkSlide() {
+    sliderImages.forEach(sliderImage => {
+        // half way through the image
+        const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+        // bottom of the image
+        // Changed the '-' to '='
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+        // Changed the '-' to '='
+        const isHalfShown = slideInAt < sliderImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        if (isHalfShown && isNotScrolledPast) {
+            sliderImage.classList.add('active');
+        } else {
+            sliderImage.classList.remove('active')
+        }
+    });
+}
+
+window.addEventListener('scroll', debounce[checkSlide]);
